573952cf25d888fbe408ae3497206215b8da511c
Jayro Salazar <megajjks@gmail.com>
2021-08-31 08:13:05 -0500

    settings the structure

--- src/components/EditarProducto.js
+++ src/components/EditarProducto.js
@@ -2,8 +2,45 @@ import React from "react";
 
 const EditarProducto = () => {
   return (
-    <div>
-      <h1>Editar producto</h1>
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-4 font-weight-bold">
+              Editar Nuevo Producto
+            </h2>
+
+            <form>
+              <div className="form-group">
+                <label>Nombre producto</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Nombre del producto"
+                  name="nombre"
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Precio del Producto</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="Precio del producto"
+                  name="precio"
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+              >
+                Guardar Producto
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
